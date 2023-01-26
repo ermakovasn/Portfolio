88fe5a50e8b7f2e59a23525ccc9289b15afb4c73
ermakovasn <sveta.kapitsa@gmail.com>
2023-01-26 17:56:50 +0100

    add contacts

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,7 +9,7 @@ hamburger.addEventListener('click', () => {
 // будем отслеживать его по списку классов и искать актив
     menu.classList.add('active');
 });
-
+// закрыть меню кликом на другую область
 closeWindow.addEventListener('click', () => {
     // будем отслеживать его по списку классов и искать актив
         menu.classList.remove('active');
@@ -19,6 +19,17 @@ closeWindow.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
     });
+
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+// каждый элемент из переменной counters перебирается
+counters.forEach( (item, i) => {
+    // чтобы определиться к определенному элементу по номеру, когда мы это значение вытащили из html, мы его перекладываем в стиль линии
+    lines[i].style.width = item.innerHTML;
+});
+
+
 
 
   
